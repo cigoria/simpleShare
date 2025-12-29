ee9c7a7ee153be4336ab2482c19a982111a5baa3
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2025-12-29 17:24:38 +0100

    new generator

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "./static")));
 
 
 async function createRandomString(length) {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const chars = "abcdefghijklmnopqrstuvwxyz";
     let result = "";
     for (let i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
